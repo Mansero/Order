00b8fc811e10bb643cf0d30ca97c74218ce19568
Simu <simon.neuenschwander@students.fhnw.ch>
2024-11-22 14:18:11 +0100

    Frontende edited #112 #111

---
+++ src/main/java/com/example/Order/Intregration/CatalogClient.java
@@ -0,0 +1,32 @@
+package com.example.Order.Intregration;
+
+import com.example.Order.model.Book;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestClient;
+
+@Component
+public class CatalogClient {
+
+    private final RestClient restClient;
+
+    public CatalogClient(RestClient.Builder restClientBuilder) {
+        this.restClient = restClientBuilder.build();
+    }
+
+    public Book[] findBooks(String searchTerm) {
+        return restClient
+                .get()
+                .uri("http://localhost:8080/api/search?searchTerm=%s".formatted(searchTerm))
+                .retrieve()
+                .body(Book[].class);
+    }
+
+    public Book[] findAll() {
+        return restClient
+                .get()
+                .uri("http://localhost:8080/api/all")
+                .retrieve()
+                .body(Book[].class);
+    }
+
+}
--- src/main/java/com/example/Order/controllers/BookController.java
+++ src/main/java/com/example/Order/controllers/BookController.java
@@ -1,45 +1,31 @@
 package com.example.Order.controllers;
 
-import com.example.Order.model.Book;
-import com.example.Order.model.BookRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
+import com.example.Order.Intregration.CatalogClient;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
-
-@RequestMapping("books")
 @Controller
 public class BookController {
 
-    private final BookRepository bookRepository;
+    private final CatalogClient catalogClient;
 
-    @Autowired
-    public BookController(BookRepository bookRepository) {
-        this.bookRepository = bookRepository;
+    public BookController(CatalogClient catalogClient) {
+        this.catalogClient = catalogClient;
     }
 
-    @GetMapping("/all")
+    @GetMapping("books/all")
     public String getAllBooks(Model model) {
-        List<Book> books = bookRepository.findAll();
-        model.addAttribute("books", books);
+        model.addAttribute("books", catalogClient.findAll());
         return "bookList";
     }
 
-    @GetMapping("/search")
+    @GetMapping("books/search")
     public String searchBooks(Model model, String searchTerm) {
-        List<Book> books = bookRepository.searchBooks(searchTerm);
-        model.addAttribute("books", books);
+        model.addAttribute("books", catalogClient.findBooks(searchTerm));
         return "bookSearch";
     }
 
-    @ResponseStatus(HttpStatus.CREATED)
-    @PostMapping
-    void createBook(@RequestBody Book book) {
-        bookRepository.save(book);
-    }
 }
 
 
